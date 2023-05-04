cae4429315f9286572bb2456af6acf10f72c033f
mengstabketemaw <mengstabrealk28@gmail.com>
2023-05-04 13:53:57 +0300

    initial commit

--- src/main/java/com/dulcons/ogr/domain/CustomForm.java
+++ src/main/java/com/dulcons/ogr/domain/CustomForm.java
@@ -1,3 +1,58 @@
 package com.dulcons.ogr.domain;
 
-public class CustomForm {}
+import javax.persistence.*;
+
+@Entity
+public class CustomForm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private FieldTypes fieldType;
+
+    private String label;
+    private Boolean required;
+    private String placeHolder;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public FieldTypes getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(FieldTypes fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Boolean getRequired() {
+        return required;
+    }
+
+    public void setRequired(Boolean required) {
+        this.required = required;
+    }
+
+    public String getPlaceHolder() {
+        return placeHolder;
+    }
+
+    public void setPlaceHolder(String placeHolder) {
+        this.placeHolder = placeHolder;
+    }
+}
