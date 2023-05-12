60fdf6494957f9805be35b086a53fc2b524a3bbd
mengstabketemaw <mengstabrealk28@gmail.com>
2023-05-12 15:05:01 +0300

    Add compliance monitoring end points

---
+++ src/main/java/com/dulcons/ogr/domain/Compliance.java
@@ -0,0 +1,51 @@
+package com.dulcons.ogr.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Compliance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    User company;
+
+    @OneToOne
+    CustomForm customForm;
+
+    String status = "Non-Compliant";
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getCompany() {
+        return company;
+    }
+
+    public void setCompany(User company) {
+        this.company = company;
+    }
+
+    public CustomForm getCustomForm() {
+        return customForm;
+    }
+
+    public void setCustomForm(CustomForm customForm) {
+        this.customForm = customForm;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/dulcons/ogr/web/rest/vm/ComplianceBody.java
@@ -0,0 +1,23 @@
+package com.dulcons.ogr.web.rest.vm;
+
+public class ComplianceBody {
+
+    Long userId;
+    Long formId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getFormId() {
+        return formId;
+    }
+
+    public void setFormId(Long formId) {
+        this.formId = formId;
+    }
+}
