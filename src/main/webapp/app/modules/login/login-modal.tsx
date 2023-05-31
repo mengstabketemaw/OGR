dbc8520cf1151d774a79aa30a845a8c2547c622b
Nathanael Bogale <natibogale45@gmail.com>
2023-05-31 16:47:48 +0300

    fixed some UI issues

--- src/main/webapp/app/modules/login/login-modal.tsx
+++ src/main/webapp/app/modules/login/login-modal.tsx
@@ -82,16 +82,16 @@ const LoginModal = (props: ILoginModalProps) => {
               </Col>
             </Row>
             <div className="mt-1">&nbsp;</div>
-            <Alert className={'bg-warning'} color="blue">
-              <Link className={'text-white'} to="/account/reset/request" data-cy="forgetYourPasswordSelector">
+            <Alert className={'bg-warnin'} color="blue">
+              <Link className={'text-blue font-weight-bold'} to="/account/reset/request" data-cy="forgetYourPasswordSelector">
                 <Translate contentKey="login.password.forgot">Did you forget your password?</Translate>
               </Link>
             </Alert>
-            <Alert className={'bg-gradient-success'} color="blue">
+            <Alert className={'bg-gradient-succes'} color="blue">
               <span>
                 <Translate contentKey="global.messages.info.register.noaccount">You don&apos;t have an account yet?</Translate>
               </span>{' '}
-              <Link className={'text-white'} to="/account/register">
+              <Link className={'text-blue font-weight-bold'} to="/account/register">
                 <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
               </Link>
             </Alert>
