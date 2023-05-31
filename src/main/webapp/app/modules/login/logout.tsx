5532ea7d6294abdce6d8bcbb9c6e2bab065cbcbc
Nathanael Bogale <natibogale45@gmail.com>
2023-05-31 09:23:24 +0300

    fixed signout issue

--- src/main/webapp/app/modules/login/logout.tsx
+++ src/main/webapp/app/modules/login/logout.tsx
@@ -2,21 +2,27 @@ import React, { useLayoutEffect } from 'react';
 
 import { useAppDispatch, useAppSelector } from 'app/config/store';
 import { logout } from 'app/shared/reducers/authentication';
+import { Storage } from 'react-jhipster';
+import Login from 'app/modules/login/login';
+import { useNavigate } from 'react-router-dom';
 
 export const Logout = () => {
   const logoutUrl = useAppSelector(state => state.authentication.logoutUrl);
   const dispatch = useAppDispatch();
+  const nav = useNavigate();
+
+  const AUTH_TOKEN_KEY = 'jhi-authenticationToken';
 
   useLayoutEffect(() => {
+    if (Storage.session.get(AUTH_TOKEN_KEY)) {
+      Storage.session.remove(AUTH_TOKEN_KEY);
+      sessionStorage.removeItem(AUTH_TOKEN_KEY);
+    }
     dispatch(logout());
-    window.location.href = '/';
+    // window.location.href = '/login';
   });
 
-  return (
-    <div className="p-5">
-      <h4>Logged out successfully!</h4>
-    </div>
-  );
+  return <>{nav('/login')}</>;
 };
 
 export default Logout;
