e26b0509902e92ae6ca7d9a37c46b37bedeb12be
betselotaderaw <betselotaderaw@gmail.com>
2023-06-12 12:38:21 +0300

    amen

--- src/main/webapp/app/modules/home/showAmendmentModal.jsx
+++ src/main/webapp/app/modules/home/showAmendmentModal.jsx
@@ -3,10 +3,35 @@ import {Button, Modal} from "reactstrap";
 import {Translate, ValidatedField} from "react-jhipster";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCloud, faWarning} from '@fortawesome/free-solid-svg-icons';
+import {updateAmendment, updateStatusAndState} from "app/modules/administration/workflow/workflow.reducer";
+import {useAppDispatch} from "app/config/store";
+import {useNavigate} from "react-router-dom";
 export const ShowAmendmentModal = (props) => {
 
-  const { handleClose, showModal, content } = props;
+  const { handleClose, showModal, content, id } = props;
+  const dispatch = useAppDispatch();
+  const nav = useNavigate()
+  const handl = () =>{
+    const param = {
+      id : id,
+      data : {
+        stateId : 0,
+        status:'Inprogress'
+      }
+    }
+    const params = {
+      id : id,
+      data : {
+        amendment : ''
+      }
+    }
+    dispatch(updateAmendment(params))
+    dispatch(updateStatusAndState(param)).then(()=>{
+      nav(0)
+      handleClose()
+    })
 
+  }
 
   return(
     <Modal isOpen={showModal} toggle={handleClose}  size={"md"}   className="modal-dialog-centered text-dark"
@@ -34,7 +59,17 @@ export const ShowAmendmentModal = (props) => {
           />
         </div>
       </div>
+      <div className="modal-footer">
+        <Button className={'bg-translucent-dark text-black'} onClick={handleClose
+        } ><Translate contentKey={'home.ignore'}/>
 
+        </Button>
+        <Button className={'bg-translucent-success text-success'} onClick={handl
+        } >
+
+          <Translate contentKey={'home.acceptAmendment'}/>
+        </Button>
+      </div>
 
     </Modal>
 
