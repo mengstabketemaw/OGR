aa65810bf05df6eb249885af638ec6fc45a76439
mengstabketemaw <mengstabrealk28@gmail.com>
2023-07-20 09:23:31 +0300

    change swagger index html. doc doesn't work on build case.

--- src/main/webapp/app/modules/administration/docs/docs.tsx
+++ src/main/webapp/app/modules/administration/docs/docs.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const DocsPage = () => (
   <div>
     <iframe
-      src="../swagger-ui/index.html"
+      src="../swagger-ui/swaggerIndex.html"
       width="100%"
       height="800"
       title="Swagger UI"
